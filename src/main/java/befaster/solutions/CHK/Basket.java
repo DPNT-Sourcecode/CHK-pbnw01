e0b4d0ed8effcb08027758bc7282d70d12acd00f
sbx_user1051 <sbx_user1051@169.254.73.173>
2024-06-08 14:22:24 +0000

    Sat Jun 08 14:22:24 UTC 2024

--- src/main/java/befaster/solutions/CHK/Basket.java
+++ src/main/java/befaster/solutions/CHK/Basket.java
@@ -6,13 +6,19 @@ import java.util.Map;
 public class Basket {
 
     private Map<Sku, Integer> basket;
+    private int totalCost;
 
     public Basket() {
         basket = new HashMap<Sku, Integer>();
+        totalCost = 0;
     }
 
     public Map<Sku, Integer> getBasket() {
         return basket;
     }
 
+    
+
+
 }
+
