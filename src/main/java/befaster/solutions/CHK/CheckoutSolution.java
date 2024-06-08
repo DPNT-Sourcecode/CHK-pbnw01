733af14a881c11d6ec8dc0c38880177d8d648cef
sbx_user1051 <sbx_user1051@169.254.73.173>
2024-06-08 15:22:08 +0000

    Sat Jun 08 15:22:08 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,64 +7,70 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
 
-    private
-
-    // Variables
-    Map<Sku, Integer> basket;
-    int totalCost;
-
-    // Constants
-    final char MIN_ITEM_CHAR = 'A';
-    final char MAX_ITEM_CHAR = 'E';
-
-    public CheckoutSolution() {
-        basket = new HashMap<Sku, Integer>();
-        totalCost = 0;
-    }
-
     public Integer checkout(String skus) {
-        // totalCost = 0;
-        char[] skusArray = skus.toCharArray();
-
-        if (!addItemsToBasket(skusArray)) {
-            // Error - invalid sku in String
-            return Integer.valueOf(-1);
-        }
-
-        // Calculate the totalCost (taking into account special offers)
-        int totalCost = ItemPriceCalculator.calculateTotalCost(basket);
-
-        return Integer.valueOf(totalCost);
+        Checkout checkout = new Checkout();
+        return checkout.calculateTotalCost(skus);
     }
 
-    // Returns true if all items are valid; false otherwise
-    private boolean addItemsToBasket(char[] skusArray) {
+    public class Checkout {
 
-        for (int i = 0; i < skusArray.length; i++) {
-            if (skusArray[i] < MIN_ITEM_CHAR || skusArray[i] > MAX_ITEM_CHAR) {
-                // Error - invalid item
-                return false;
-            }
-            switch (skusArray[i]) {
-                case 'A':
-                    basket.merge(Sku.A, 1, Integer::sum);
-                    break;
-                case 'B':
-                    basket.merge(Sku.B, 1, Integer::sum);
-                    break;
-                case 'C':
-                    basket.merge(Sku.C, 1, Integer::sum);
-                    break;
-                case 'D':
-                    basket.merge(Sku.D, 1, Integer::sum);
-                    break;
-                case 'E':
-                    basket.merge(Sku.E, 1, Integer::sum);
-                    break;
-            }
+        // Variables
+        Map<Sku, Integer> basket;
+        int totalCost;
+
+        // Constants
+        final char MIN_ITEM_CHAR = 'A';
+        final char MAX_ITEM_CHAR = 'E';
+
+        public Checkout() {
+            basket = new HashMap<Sku, Integer>();
+            totalCost = 0;
         }
-        return true;
+
+        public Integer calculateTotalCost(String skus) {
+            char[] skusArray = skus.toCharArray();
+    
+            if (!addItemsToBasket(skusArray)) {
+                // Error - invalid sku in String
+                return Integer.valueOf(-1);
+            }
+    
+            // Calculate the totalCost (taking into account special offers)
+            int totalCost = ItemPriceCalculator.calculateTotalCost(basket);
+    
+            return Integer.valueOf(totalCost);
+        }
+
+        // Returns true if all items are valid; false otherwise
+        private boolean addItemsToBasket(char[] skusArray) {
+
+            for (int i = 0; i < skusArray.length; i++) {
+                if (skusArray[i] < MIN_ITEM_CHAR || skusArray[i] > MAX_ITEM_CHAR) {
+                    // Error - invalid item
+                    return false;
+                }
+                switch (skusArray[i]) {
+                    case 'A':
+                        basket.merge(Sku.A, 1, Integer::sum);
+                        break;
+                    case 'B':
+                        basket.merge(Sku.B, 1, Integer::sum);
+                        break;
+                    case 'C':
+                        basket.merge(Sku.C, 1, Integer::sum);
+                        break;
+                    case 'D':
+                        basket.merge(Sku.D, 1, Integer::sum);
+                        break;
+                    case 'E':
+                        basket.merge(Sku.E, 1, Integer::sum);
+                        break;
+                }
+            }
+            return true;
+        }
     }
 
 }
+
 
