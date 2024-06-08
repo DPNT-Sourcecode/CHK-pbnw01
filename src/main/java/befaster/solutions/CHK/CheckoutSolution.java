d84c4d745f11cfe40c53f563efde7b1de81b931e
sbx_user1051 <sbx_user1051@169.254.73.173>
2024-06-08 14:04:41 +0000

    Sat Jun 08 14:04:41 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -34,13 +34,37 @@ public class CheckoutSolution {
 
         int totalCost = 0;
 
+        while (numA > 0) {
+            if (numA >= 3) {
+                totalCost += 130;
+                numA -= 3;
+            } else {
+                totalCost += 50;
+                numA--;
+            }
+        }
+
+        while (numB > 0) {
+            if (numB >= 2) {
+                totalCost += 45;
+                numB -= 2;
+            } else {
+                totalCost += 30;
+                numB--;
+            }
+        }
+
+        totalCost += (numC * 20);
+        totalCost += (num)
+
         return -1;
     }
 
-    
+
 
 
 }
+
 
 
 
